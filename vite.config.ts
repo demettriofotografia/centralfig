5dcc944ca0b21b85f7ab39f115423b968eb237df
demettriofotografia <demettriofotografia@gmail.com>
2026-04-21 20:14:23 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,10 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// Minimalist config to maximize compatibility with Vercel/Cloud builds
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  },
   server: {
     port: 3000,
     host: '0.0.0.0',
-  }
+  },
 });
